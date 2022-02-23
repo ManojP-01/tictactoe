8e188baf6f69524c907f3fa6e4b93cd88d1b9e81
manoj1509 <manojprakasam2001@gmail.com>
2022-02-23 16:57:25 +0545

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Board from './components/Board';
+import './styles/root.scss';
 
 const App = () => (
-  <>
+  <div className="app">
     <h1>Welcome to React Vite Micro App!</h1>
     <p>Hard to get more minimal than this React app.</p>
     <Board />
-  </>
+  </div>
 );
 export default App;
