b666cb9b1876ece07c451f9c9807a3a5560c6eb7
manoj1509 <manojprakasam2001@gmail.com>
2022-02-23 13:45:23 +0545

    created board and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
+import Board from './components/Board';
 
 const App = () => (
   <>
     <h1>Welcome to React Vite Micro App!</h1>
     <p>Hard to get more minimal than this React app.</p>
+    <Board />
   </>
 );
 export default App;
